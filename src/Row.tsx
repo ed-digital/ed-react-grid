8348b1043a68f47c0604d9346ab2be8ab12a6579
Daniel Lever <daniel@ed.com.au>
2019-03-01 14:48:01 +1100

    Not sure :)

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { row, columnPadding } from './generators'
 import { basicRowStyle } from './styles'
 import { VerticalGutterPaddingProps, StyledProps } from './types'
 import { default as Grid, WrapperContext } from './Grid'
+
 export type RowProps = VerticalGutterPaddingProps & {
   children: React.ReactNode
+  noWrap?: true
 }
 
 export default function Row(props: RowProps) {
@@ -18,4 +20,13 @@ export const RowStyle = styled.div`
   ${basicRowStyle}
   ${row()}
   ${(props: RowProps & StyledProps) => columnPadding(props)(props)}
+  ${props =>
+    props.noWrap &&
+    css`
+      white-space: nowrap;
+
+      > div {
+        white-space: auto;
+      }
+    `}
 `
